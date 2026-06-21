d8dfb11ca95f77fa70a6a4341d1609a8136d474f
VivekSai07 <viveksaisurya07@gmail.com>
2026-06-21 10:24:57 +0200

    Modified: train_rgbd.py

--- il/baselines/act/train_rgbd.py
+++ il/baselines/act/train_rgbd.py
@@ -353,6 +353,7 @@ class Agent(nn.Module):
         self.state_dim = env.single_observation_space['state'].shape[0]
         self.act_dim = env.single_action_space.shape[0]
         self.kl_weight = args.kl_weight
+        self.include_depth = args.include_depth
         self.normalize = T.Normalize(mean=[0.485, 0.456, 0.406],
                                      std=[0.229, 0.224, 0.225])
 
@@ -383,7 +384,7 @@ class Agent(nn.Module):
         obs['rgb'] = self.normalize(obs['rgb'])
 
         # depth data
-        if args.include_depth:
+        if self.include_depth:
             obs['depth'] = obs['depth'].float()
 
         # forward pass
@@ -407,7 +408,7 @@ class Agent(nn.Module):
         obs['rgb'] = self.normalize(obs['rgb'])
 
         # depth data
-        if args.include_depth:
+        if self.include_depth:
             obs['depth'] = obs['depth'].float()
 
         # forward pass
